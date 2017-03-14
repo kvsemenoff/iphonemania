ad59b727f8a70f3e1db247acef7f070421641fb9
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-03-14 20:03:19 +0300

    nothing

--- includes/our.php
+++ includes/our.php
@@ -1,4 +1,13 @@
 <section class="section section_padding our">
+<div class="col-md-7 our-right hidden-md hidden-lg">
+			<div class="container">
+				<h2 class="our-title hidden-lg hidden-md">Наши преимущества</h2>
+				<div class="our-item our-item1"><div class="our-img-wrap"><img src="img/our-item1.png" alt="" class="our-img"></div><span class="our-item-text">На все телефоны 1 год гарантии.</span></div>
+				<div class="our-item our-item2"><div class="our-img-wrap"><img src="img/our-item2.png" alt="" class="our-img"></div><span class="our-item-text">Работаем более 7-ми лет. Мы надежны  и не пропадем как новые магазины.</span></div>
+				<div class="our-item our-item3"><div class="our-img-wrap"><img src="img/our-item3.png" alt="" class="our-img"></div> <span class="our-item-text">Гарантийное обслуживание проходит  в собственном сервисном центре.</span></div>
+				<div class="our-item our-item4"><div class="our-img-wrap"><img src="img/our-item4.png" alt="" class="our-img"></div><span class="our-item-text">Ремонт обычно занимает не больше одной недели.</span></div>
+			</div>
+</div>
 <div class=" container">
 	<div class="row">
 		<div class="col-md-5 our-left hidden-xs hidden-sm">
@@ -26,14 +35,16 @@
 				            <a href="#" class="button1 our-btn">Заказать</a>
 			</div>
         </div>
-		<div class="col-md-7 our-right">
+		<div class="col-md-7 our-right hidden-xs hidden-sm">
 			<h2 class="our-title hidden-lg hidden-md">Наши преимущества</h2>
 			<div class="our-item our-item1"><div class="our-img-wrap"><img src="img/our-item1.png" alt="" class="our-img"></div><span class="our-item-text">На все телефоны 1 год гарантии.</span></div>
 			<div class="our-item our-item2"><div class="our-img-wrap"><img src="img/our-item2.png" alt="" class="our-img"></div><span class="our-item-text">Работаем более 7-ми лет. Мы надежны  и не пропадем как новые магазины.</span></div>
 			<div class="our-item our-item3"><div class="our-img-wrap"><img src="img/our-item3.png" alt="" class="our-img"></div> <span class="our-item-text">Гарантийное обслуживание проходит  в собственном сервисном центре.</span></div>
 			<div class="our-item our-item4"><div class="our-img-wrap"><img src="img/our-item4.png" alt="" class="our-img"></div><span class="our-item-text">Ремонт обычно занимает не больше одной недели.</span></div>
 		</div>
-		<div class="col-md-5  our-left hidden-md hidden-lg">
+	</div>
+</div>
+<div class="col-md-5  our-left hidden-md hidden-lg">
 			<div class="our-wrap">
 				<div class="dd-timer dd-timer2">
 					            <span class="dd-timer-title">успей сделать заказ</span>
@@ -57,6 +68,4 @@
 				            <a href="#" class="button1 our-btn">Заказать</a>
 			</div>
         </div>
-	</div>
-</div>
 </section>
