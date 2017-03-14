5bb107964fa097d4c637ff5d1a7e1ff4ffe466c3
Barasbi <betokov93@gmail.com>
2017-03-14 13:10:24 +0300

    add number #1

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,21 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding db-section_footer-view">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-6">
+				<p>
+				<img src="img/db-flag.png" alt="img">
+					2-ая линия Васильевского острова 61/30, <br>
+					Василеостровский район, <br>
+					Санкт-Петербург, Россия 
+				</p>
+			</div>
+			<div class="col-md-6">
+			<p>
+				<img src="img/" alt="img">
+					<span>+7 812 380-40-11</span><br>
+					Сделано в<a href="#"><img src="img" alt="img"></a>
+				</p>
+			</div>
 		</div>
 	</div>
-</div>
 </section>
