e3eef33489503945028e4f2c6bd0671065a39cfb
Barasbi <betokov93@gmail.com>
2017-03-14 17:16:38 +0300

    add number #1

--- includes/fast.php
+++ includes/fast.php
@@ -1,7 +1,13 @@
-<section class="section section_padding">
-<div class=" container">
+<section class="section section_padding section_db-fast-view">
+<div class=" container db-all-fast">
 	<div class="row">
 		<div class="col-md-12">
+		<h1 class="db-fast-h1">Быстрый заказ</h1>
+		<form action="#" method="post" class="db-fast-form">
+			<input type="text" name="uname" placeholder="Имя"><br>
+			<input type="text" name="phone" placeholder="Телефон" class="js-phone">
+			<input type="submit" name="submit" value="Заказать" class="button1">
+		</form>
 		</div>
 	</div>
 </div>
