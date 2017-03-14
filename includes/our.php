3ae87c16f0dc7224e36d549f34f8119f919d2dfe
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-03-14 17:37:31 +0300

    point

--- includes/our.php
+++ includes/our.php
@@ -1,7 +1,7 @@
 <section class="section section_padding our">
 <div class=" container">
 	<div class="row">
-		<div class="col-md-5 our-left">
+		<div class="col-md-5 our-left hidden-xs hidden-sm">
 			<h2 class="our-title">Наши преимущества</h2>
 			<div class="col-md-12 our-wrap">
 				<div class="dd-timer dd-timer2">
@@ -27,11 +27,36 @@
 			</div>
         </div>
 		<div class="col-md-7 our-right">
-			<div class="our-item our-item1">На все телефоны 1 год гарантии.</div>
-			<div class="our-item our-item2">Работаем более 7-ми лет. Мы надежны  и не пропадем как новые магазины.</div>
-			<div class="our-item our-item3"> Гарантийное обслуживание проходит  в собственном сервисном центре.</div>
-			<div class="our-item our-item4">Ремонт обычно занимает не больше одной недели.</div>
+			<h2 class="our-title hidden-lg hidden-md">Наши преимущества</h2>
+			<div class="our-item our-item1"><div class="our-img-wrap"><img src="img/our-item1.png" alt="" class="our-img"></div><span class="our-item-text">На все телефоны 1 год гарантии.</span></div>
+			<div class="our-item our-item2"><div class="our-img-wrap"><img src="img/our-item2.png" alt="" class="our-img"></div><span class="our-item-text">Работаем более 7-ми лет. Мы надежны  и не пропадем как новые магазины.</span></div>
+			<div class="our-item our-item3"><div class="our-img-wrap"><img src="img/our-item3.png" alt="" class="our-img"></div> <span class="our-item-text">Гарантийное обслуживание проходит  в собственном сервисном центре.</span></div>
+			<div class="our-item our-item4"><div class="our-img-wrap"><img src="img/our-item4.png" alt="" class="our-img"></div><span class="our-item-text">Ремонт обычно занимает не больше одной недели.</span></div>
 		</div>
+		<div class="col-md-5  our-left hidden-md hidden-lg">
+			<div class="our-wrap">
+				<div class="dd-timer dd-timer2">
+					            <span class="dd-timer-title">успей сделать заказ</span>
+					            <div class="dd-times">
+					                <div class="dd-div">
+					                    <span class="dd-hours dd-hours1">0</span>
+					                    <span class="dd-hours dd-hours2">0</span>
+					                    <br>
+					                </div>
+					                <div class="dd-div dd-div-margin">
+					                    <span class="dd-min dd-min1">1</span>
+					                    <span class="dd-min dd-min2">1</span><br>
+					                </div>
+					                <div class="dd-div dd-div-margin">
+					                    <span class="dd-sec dd-sec1">1</span>
+					                    <span class="dd-sec dd-sec2">7</span><br>
+					                </div>
+					            </div>
+					            <span class="dd-timer-bot">и получи подарок!</span>
+				            </div>
+				            <a href="#" class="button1 our-btn">Заказать</a>
+			</div>
+        </div>
 	</div>
 </div>
 </section>
--- includes/point.php
+++ includes/point.php
@@ -1,8 +1,30 @@
-<section class="section section_padding">
-<div class=" container">
+<section class="section section_padding delivery">
+<div class="container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-5 delivery-left"></div>
+		<div class="col-md-7 delivery-right">
+			<div class="delivery-item">
+				<div class="delivery-imgwrap delivery-imgwrap1"></div>
+				<p class="delivery-text delivery-text1">Точка выдачи заказов в центре СПб  на Невском Проспекте. Удобная развязка.</p>
+			</div>
+			<div class="delivery-item">
+				<div class="delivery-imgwrap delivery-imgwrap2"></div>
+				<p class="delivery-text delivery-text2">Магазин работает каждый день с 10 до 21.</p>
+			</div>
+			<div class="delivery-item">
+				<div class="delivery-imgwrap delivery-imgwrap3"></div>
+				<p class="delivery-text">Если Вам необходима доставка, то она  осуществляется в этот же день.</p>
+			</div>
+			<div class="delivery-item">
+				<div class="delivery-imgwrap delivery-imgwrap4"></div>
+				<p class="delivery-text">Все модели представленные на сайте есть  в наличии на складе.</p>
+			</div>
+			<div class="delivery-item">
+				<div class="delivery-imgwrap delivery-imgwrap5"></div>
+				<p class="delivery-text">Обрабатываем Вашу заявку течение 30 минут,  после нажатия на кнопку отправить.</p>
+			</div>
 		</div>
+		<div class="col-sm-12 hidden-md hidden-lg delivery-bottom"></div>
 	</div>
 </div>
 </section>
