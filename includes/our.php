86b3304ad645ffeb78a3a9653189f7e39ffd20ce
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-03-14 14:08:38 +0300

    tech.php

--- includes/our.php
+++ includes/our.php
@@ -1,7 +1,36 @@
-<section class="section section_padding">
+<section class="section section_padding our">
 <div class=" container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-5 our-left">
+			<h2 class="our-title">Наши преимущества</h2>
+			<div class="col-md-12 our-wrap">
+				<div class="dd-timer dd-timer2">
+					            <span class="dd-timer-title">успей сделать заказ</span>
+					            <div class="dd-times">
+					                <div class="dd-div">
+					                    <span class="dd-hours dd-hours1">0</span>
+					                    <span class="dd-hours dd-hours2">0</span>
+					                    <br>
+					                </div>
+					                <div class="dd-div dd-div-margin">
+					                    <span class="dd-min dd-min1">1</span>
+					                    <span class="dd-min dd-min2">1</span><br>
+					                </div>
+					                <div class="dd-div dd-div-margin">
+					                    <span class="dd-sec dd-sec1">1</span>
+					                    <span class="dd-sec dd-sec2">7</span><br>
+					                </div>
+					            </div>
+					            <span class="dd-timer-bot">и получи подарок!</span>
+				            </div>
+				            <a href="#" class="button1 our-btn">Заказать</a>
+			</div>
+        </div>
+		<div class="col-md-7 our-right">
+			<div class="our-item our-item1">На все телефоны 1 год гарантии.</div>
+			<div class="our-item our-item2">Работаем более 7-ми лет. Мы надежны  и не пропадем как новые магазины.</div>
+			<div class="our-item our-item3"> Гарантийное обслуживание проходит  в собственном сервисном центре.</div>
+			<div class="our-item our-item4">Ремонт обычно занимает не больше одной недели.</div>
 		</div>
 	</div>
 </div>
