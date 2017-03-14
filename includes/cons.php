25c34e116c5bbebe3a943cf859e7567f81929796
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-03-14 19:56:00 +0300

    nothing

--- includes/cons.php
+++ includes/cons.php
@@ -1,8 +1,28 @@
-<section class="section section_padding">
+<section class="section section_padding cons">
+<div class="col-md-6 cons-right hidden-md hidden-lg">
+			<div class="cons-text-block">
+				<p class="cons-text">Консультанты в магазине Всегда 
+					окажут консультации, зарегистрируют 
+					Apple ID или расскажут о всех 
+					возможностях Iphone, в том числе 
+					скрытых. 
+				</p>
+			</div>
+		</div>
 <div class=" container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-6"></div>
+		<div class="col-md-6 cons-right hidden-xs hidden-sm">
+			<div class="cons-text-block">
+				<p class="cons-text">Консультанты в магазине Всегда 
+					окажут консультации, зарегистрируют 
+					Apple ID или расскажут о всех 
+					возможностях Iphone, в том числе 
+					скрытых. 
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
+<div class="hidden-md hidden-lg cons-hidden"></div>
 </section>
--- includes/nothing.php
+++ includes/nothing.php
@@ -1,7 +1,21 @@
-<section class="section section_padding">
+<section class="section section_padding nothing">
 <div class=" container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-6 nothing-left">
+			<div class="nothing-text-block hidden-md hidden-lg">
+				<p class="nothing-title">Нам нечего скрывать</p>
+				<p class="nothing-text">Вы можете вскрыть упаковку и проверить 
+				Ваш новый Iphone на работоспособность. 
+				Залогиниться в своем Apple ID</p>
+			</div>
+		</div>
+		<div class="col-md-6 nothing-right">
+			<div class="nothing-text-block hidden-xs hidden-sm">
+				<p class="nothing-title">Нам нечего скрывать</p>
+				<p class="nothing-text">Вы можете вскрыть упаковку и проверить 
+				Ваш новый Iphone на работоспособность. 
+				Залогиниться в своем Apple ID</p>
+			</div>
 		</div>
 	</div>
 </div>
