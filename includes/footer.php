d068f9cfc687139d062cc59799b2ce3058bba183
Barasbi <betokov93@gmail.com>
2017-03-17 12:29:30 +0300

    add number #1

--- includes/footer.php
+++ includes/footer.php
@@ -1,20 +1,44 @@
 <section class="section section_padding db_footer-view">
 	<div class=" container">
 		<div class="row">
-				<div class="col-md-6">
-					<p class="db-footer_p_one">
-						2-ая линия Васильевского острова 61/30, <br>
-						Василеостровский район, <br>
-						Санкт-Петербург, Россия 
-					</p>
-				</div>
-				<div class="col-md-6">
-					<p class="db-footer_p_two">
-						<span class="footer-span-one">+7 812 380-40-11</span><br>
-						<span class="footer-span-two">Сделано в </span><a href="#"><img src="img/db-studia.png" alt="img"></a>
-						<div class="clearfix"></div>
-					</p>
-				</div>
+			<div class="col-md-6">
+				<p class="db-footer_p_one">
+					2-ая линия Васильевского острова 61/30, <br>
+					Василеостровский район, <br>
+					Санкт-Петербург, Россия 
+				</p>
+			</div>
+			<div class="col-md-6">
+				<p class="db-footer_p_two">
+					<span class="footer-span-one">+7 812 380-40-11</span><br>
+					<span class="footer-span-two">Сделано в </span><a href="#"><img src="img/db-studia.png" alt="img"></a>
+					<div class="clearfix"></div>
+				</p>
+			</div>
 		</div>
 	</div>
 </section>
+
+<!-- MASK -->
+<div class="mask" id="js-mask"></div>
+<!-- POPAPS -->
+<div class="js-window" id="js-form1">
+	<div class="db-form1">
+		<h1 class="db-fast-h1">Быстрый заказ</h1>
+		<img src="../img/kres.png" alt="img" class="kres js-cres">
+		<form action="#" method="post" class="db-fast-form js-submit">
+			<input type="text" name="uname" placeholder="Имя"><br>
+			<input type="text" name="phone" placeholder="Телефон" class="js-phone">
+			<input type="submit" name="submit" value="Заказать" class="button1">
+		</form>
+	</div>
+</div>
+<div class="js-window" id="js-form2">
+	<div class="db-form2">
+	<img src="../img/kres.png" alt="img" class="kres2 js-cres">
+	<h1 class="db-fast-h1">Спасибо за заявку! </h1>
+			<p>Наш менеджер свяжется с вами.</p>
+		</div>
+	</div>
+<a href="#js-form1" name="js-modal">FORM1</a>
+<a href="#js-form2" name="js-modal"></a>
